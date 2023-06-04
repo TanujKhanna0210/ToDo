d942422b07fc1d0cf879912070f45b8dcc41fbc6
Tanuj Khanna <tanujkhanna72@gmail.com>
2023-06-04 19:57:03 +0530

    Implementing sign up functionality

--- app/src/main/java/com/example/todo/fragments/SignUpFragment.kt
+++ app/src/main/java/com/example/todo/fragments/SignUpFragment.kt
@@ -5,25 +5,70 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.example.todo.R
+import com.example.todo.databinding.FragmentSignUpBinding
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.FirebaseAuth
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SignUpFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SignUpFragment : Fragment() {
+
+    private lateinit var binding: FragmentSignUpBinding
+    private lateinit var auth: FirebaseAuth
+    private lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+        binding = FragmentSignUpBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        init(view)
+        registerEvents()
+    }
+
+    private fun init(view: View) {
+        navController = Navigation.findNavController(view)
+        auth = FirebaseAuth.getInstance()
+    }
+
+    private fun registerEvents() {
+
+        binding.signIn.setOnClickListener {
+            navController.navigate(R.id.action_signUpFragment_to_signInFragment)
+        }
+
+        binding.nextBtn.setOnClickListener {
+            val email = binding.emailEt.text.toString().trim()
+            val password = binding.passwordEt.text.toString().trim()
+            val verifyPassword = binding.retypePasswordEt.text.toString().trim()
+
+            if(email.isNotEmpty() && password.isNotEmpty() && verifyPassword.isNotEmpty()) {
+                if(password == verifyPassword) {
+                    auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(
+                        OnCompleteListener {
+                            if(it.isSuccessful) {
+                                Toast.makeText(context, "Registered successfully", Toast.LENGTH_SHORT).show()
+                                navController.navigate(R.id.action_signUpFragment_to_homeFragment)
+                            }else{
+                                Toast.makeText(context, it.exception?.message, Toast.LENGTH_SHORT).show()
+                            }
+                        })
+                }else{
+                    Toast.makeText(context, "Password is not matching!", Toast.LENGTH_SHORT).show()
+                }
+            }else{
+                Toast.makeText(context, "Empty fields are not allowed!", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
