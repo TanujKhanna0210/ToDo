9d2c9fd8b4e71b089701f3c9bafcf211bc83f858
Tanuj Khanna <tanujkhanna72@gmail.com>
2023-06-05 08:50:39 +0530

    Adding tasks to database

--- app/src/main/java/com/example/todo/fragments/TaskPopupFragment.kt
+++ app/src/main/java/com/example/todo/fragments/TaskPopupFragment.kt
@@ -5,29 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.DialogFragment
 import com.example.todo.R
+import com.example.todo.databinding.FragmentTaskPopupBinding
+import com.google.android.material.textfield.TextInputEditText
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class TaskPopupFragment : DialogFragment() {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [TaskPopupFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class TaskPopupFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var binding: FragmentTaskPopupBinding
+    private lateinit var listener: DialogNextBtnClickListener
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    fun setListener(listener: DialogNextBtnClickListener){
+        this.listener = listener
     }
 
     override fun onCreateView(
@@ -35,26 +25,32 @@ class TaskPopupFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_task_popup, container, false)
+        binding = FragmentTaskPopupBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment TaskPopupFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            TaskPopupFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        registerEvents()
+    }
+
+    private fun registerEvents() {
+        binding.nextBtn.setOnClickListener {
+            val task = binding.taskEt.text.toString()
+
+            if (task.isNotEmpty()) {
+                listener.onSaveTask(task, binding.taskEt)
+            } else {
+                Toast.makeText(context, "Please enter a task!", Toast.LENGTH_SHORT).show()
             }
+        }
+
+        binding.closeBtn.setOnClickListener {
+            dismiss()
+        }
+    }
+    interface DialogNextBtnClickListener{
+        fun onSaveTask(task: String, taskEt: TextInputEditText)
     }
 }
